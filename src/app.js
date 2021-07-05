1ecdf7e24b5a79e6734e8d4b0405049348c15568
Banushasarma <sharmabanu03@gmail.com>
2021-07-05 20:52:56 +0530

    environment setup

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const geocode = require('./utils/geocode');
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //Define path directories for express
 const publicDir = path.join(__dirname, '../public')
 const viewsDir = path.join(__dirname, '../templates/views')
@@ -99,6 +101,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000');
 })
